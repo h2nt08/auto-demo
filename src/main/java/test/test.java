83dbdde5ece3a3b4bf2db1578aa30417401b9304
hainh <hainh@exoplatform.com>
2013-08-09 15:16:14 +0700

    auto demo project

---
+++ src/main/java/test/test.java
@@ -0,0 +1,29 @@
+package test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+public class test {
+ public WebDriver driver;
+	
+ @BeforeMethod
+ public void beforeTest(){
+	 
+ }
+ @AfterMethod
+ public void afterTest(){
+	 
+ }
+
+ @Test
+ public void test01(){
+//	 FirefoxDriver driver = new FirefoxDriver();
+	 System.setProperty("webdriver.ie.driver", "C:\\workspace\\auto-demo\\IEDriverServer.exe");
+	 DesiredCapabilities caps = DesiredCapabilities.internetExplorer();
+	 caps.setCapability("ignoreZoomSetting", true);
+	 driver = new InternetExplorerDriver(caps);
+	 driver.get("www.google.com.vn");
+ }
+}
